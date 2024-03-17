aba3b751bb445c85efbd04d2bd9e409a72a1746c
ipullynnhah <ipullynnhah@gmail.com>
2024-03-17 16:25:51 -0300

    fix: rewrite conditionals to feat the requirements

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ function playRockPaperScissor(player1, player2) {
     return "Empate!";
   }
   
+
   let winner;
-  if (player1 === "Pedra") {
-    winner = player2 === "Papel" ? 2 : 1;
-  } else if (player1 === "Papel") {
-    winner = player2 === "Tesoura" ? 2 : 1;
-  } else  {
-    winner = player2 === "Pedra" ? 2 : 1;
+  if (player1 === "Pedra" && player2 === "Tesoura") {
+    winner = 1;
+  } else if (player1 === "Papel" && player2 === "Pedra") {
+    winner = 1;
+  } else if (player1 === "Tesoura" && player2 === "Papel") {
+    winner = 1;
+  } else {
+    winner = 2;
   }
   
   return `Jogador ${winner} venceu!`;
