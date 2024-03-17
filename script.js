e35c467cb6c2138c1051843faf1834d8a6ba9619
ipullynnhah <ipullynnhah@gmail.com>
2024-03-17 15:41:44 -0300

    feat: implement function playRockPaperScissor w/ all tests passing

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+  if (player1 === player2) {
+    return "Empate!";
+  }
+  
+  let winner;
+  if (player1 === "Pedra") {
+    winner = player2 === "Papel" ? 2 : 1;
+  } else if (player1 === "Papel") {
+    winner = player2 === "Tesoura" ? 2 : 1;
+  } else  {
+    winner = player2 === "Pedra" ? 2 : 1;
+  }
+  
+  return `Jogador ${winner} venceu!`;
+}
